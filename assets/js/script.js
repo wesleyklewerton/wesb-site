8518a74edca6166e97aaa6a997eafae7622a79d4
Thiago Nascimento <thiagodnf@gmail.com>
2016-10-05 17:22:55 -0300

    fix small logo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 $(function(){
-    var $nav = $('#header');
+    /*var $nav = $('#header');
 
+    //console.log($nav.offset().top)
     $nav.scrollspy({
         min: $nav.offset().top,
         onEnter: function(element, position) {
@@ -12,7 +13,7 @@ $(function(){
             $("#logo-small").show();
         }
     });
-
+*/
     // for each element with the class 'color'
     $('article').each(function eachElement() {
         // cache the jQuery object
@@ -24,6 +25,7 @@ $(function(){
             max: position.top + $this.height(),
             onEnter: function onEnter(element/*, position*/) {
                 $(".current").removeClass("current");
+
                 if(element.id == "about"){
                     $('a[href="#page-wrapper"]').parent().addClass("current");
                 }else{
@@ -37,6 +39,25 @@ $(function(){
             }
         });
     });
+
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutationRecord) {
+            if($("#header").hasClass("alt")){
+                $("#logo-small").hide();
+            }else{
+                $("#logo-small").show();
+            }
+        });
+    });
+
+    var target = document.getElementById('header');
+    observer.observe(target, { attributes : true, attributeFilter : ['class'] });
+
+    $('header').on('change', function() {
+        console.log("Oi")
+        //$('#myDiv').addClass('submission-ok').trigger('classChange');
+    });
+
 
     function resizeBanner(){
         $("#banner").height($( window ).height()-220);
